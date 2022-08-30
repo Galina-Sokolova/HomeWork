bee418d9942038e3edb2d3966219429d94d64534
GalinaS <peraskevasokolova@gmail.com>
2022-08-30 21:40:49 +0400

    filling an array with index cubes

---
+++ HW25-08_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и помещает в массив таблицу кубов чисел от 1 до N.
+//Полученный массив вывести на экран.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [num];
+for (int index = 0; index < num; index++ )
+{
+    array[index] = Convert.ToInt32(Math.Pow(index, 3));;
+    Console.Write(array[index]+" ");
+}
+
