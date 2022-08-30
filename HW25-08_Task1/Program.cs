0a149b4921943809b3a609301e734429da6e0a8d
GalinaS <peraskevasokolova@gmail.com>
2022-08-30 20:19:30 +0400

    palindrome

---
+++ HW25-08_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом.
+Console.Write("Введите целое число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+//переменная для записи обратного числа
+int recNum = 0;
+int Num = input;
+while (Num != 0)
+{
+    recNum = recNum * 10 + Num % 10;
+    Num /= 10;
+}
+//Console.WriteLine(recNum);
+if (recNum == input)
+{
+    Console.WriteLine("Число "+input+" является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число "+input+" не является палиндромом");
+}
