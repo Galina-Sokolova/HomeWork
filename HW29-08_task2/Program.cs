9bb172e223c010669c7a010ac8eeff0078796e8c
GalinaS <peraskevasokolova@gmail.com>
2022-08-31 20:49:29 +0400

    the sum of the divisors of a number

---
+++ HW29-08_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, которая принимает на вход число и выдаёт
+//сумму цифр в числе, которые являются делителями этого числа.
+Console.Write("Введите число а = ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int num = A;
+while ( num != 0 )
+{
+    int rem = num % 10;
+    int rem1 = A % rem;
+    num /= 10;
+    if (rem1 == 0) 
+    {
+        sum = sum + rem;
+    }
+}
+Console.Write("сумма цифр, которые являются делителями числа "+A+" = "+sum);
+
