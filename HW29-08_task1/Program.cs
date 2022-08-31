bb70ad8c626770e2c58452abf57f9756488835ab
GalinaS <peraskevasokolova@gmail.com>
2022-08-31 20:24:26 +0400

    search cycle for the power of a number

---
+++ HW29-08_task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// цикл, который принимает на вход два числа (A и B) и возводит
+//число A в натуральную степень B. Не использовать функцию Math.Pow()
+Console.Write("Введите число а = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число b = ");
+uint B = Convert.ToUInt32(Console.ReadLine());
+int tim;
+int prod = 1;
+for (tim = 1; tim <= B; tim++)
+{
+    prod = prod * A;
+}
+Console.Write("a ^ b = "+A+" ^ "+B+" = "+prod);
