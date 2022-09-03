0693ab3ca85b1b79cd2e888973c1bec684e14d2d
GalinaS <peraskevasokolova@gmail.com>
2022-09-03 15:25:11 +0400

    сумма четных элементов на нечетных позициях

---
+++ HW01-09_task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму четных элементов, стоящих на нечётных позициях
+//(позиция - индекс элемента в массиве).
+Console.Write("Введите число - количество элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = -999;
+int stop = 1000;
+int[]array = new int[N];
+Random rnd = new Random();
+int sum = 0;
+Console.Write("Массив: ");
+for (int index = 0; index < N; index++)
+{
+    array[index] = rnd.Next(start, stop);
+    Console.Write(array[index]+" ");    
+}
+Console.WriteLine("");
+Console.Write("Сумма четных элементов: ");
+for(int index = 1; index < N; index+=2)
+{
+    int rem = array[index] % 2;
+        if (rem == 0)
+        {
+            sum += array[index];
+            Console.Write("array["+index+"] ");
+        }
+}
+Console.Write("= "+sum);
+
