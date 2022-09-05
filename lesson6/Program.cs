f62dd907375f0b7be393d00bed4b970736b4d6aa
GalinaS <peraskevasokolova@gmail.com>
2022-09-05 23:41:56 +0400

    преобразование десятичного числа в двоичное

---
+++ lesson6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[100];
+int i = 0;
+int count = 0;
+while (N > 0)
+{
+    arr[i] = N % 2;
+    N = N / 2;
+    Console.Write(arr[i]+" ");
+    i++;
+    count += 1;
+}
+Console.WriteLine(" ");
+for (i = count - 1; i >= 0; i --)
+{
+  Console.Write(arr[i]+" ");  
+}
