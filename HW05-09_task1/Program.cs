6f6d0061a149e0a4efb5f68b9cf78c47bf76be40
GalinaS <peraskevasokolova@gmail.com>
2022-09-09 21:57:31 +0400

    counting positive numbers

---
+++ HW05-09_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа запрашивает натуральное число M, пользователь
+// вводит с клавиатуры M чисел. Посчитайте, сколько 
+// чисел больше 0 ввёл пользователь.
+Console.WriteLine("Сколько чисел Вы хотите ввести?");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int i = 1;
+while (i <= M)
+{
+    Console.Write($"Введите число {i}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+    i++;
+}
+Console.Write($"Вы ввели {count} положительных чисел");
