3b17e2847ca50f53019d8f81bbc89548cc34f8e5
GalinaS <peraskevasokolova@gmail.com>
2022-09-09 23:17:54 +0400

    the sum of the products

---
+++ HW05-09_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найдите сумму произведений пар чисел в одномерном целочисленном
+//массиве. Парой считаем первый и последний элемент, второй 
+//и предпоследний и т.д. Если кол-во элементов нечетное, 
+//то элемент посередине не учитывать.Результат сложения вывести на экран.
+Console.Write("Введите число - количество элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N]; 
+Random rnd = new Random();
+for (int index = 0; index < N; index++)
+{
+    array[index] = rnd.Next(-10, 10);
+    Console.Write(array[index]+" ");
+}
+Console.WriteLine();
+int sum = 0;
+for (int index = 0; index < N/2; index++)
+{
+    int tmp = array[index] * array[N-1-index];
+    sum = sum + tmp;
+}
+Console.Write(sum);
+
